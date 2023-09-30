c2f4ebda4172db7ef752bed7d2919b4d7737eaed
Austin Ginn <austinleeginn@gmail.com>
2023-09-30 16:06:50 +0000

    started cron init

--- server.mjs
+++ server.mjs
@@ -2,6 +2,7 @@ import TCLRokuTV from "./tcl-roku.mjs";
 import JSONFileHandler from "./json-file-handler.mjs";
 import express from 'express';
 import expressAPI from './expressAPI.mjs';
+import cron from 'node-cron';
 
 const app = express();
 const port = 3000;
@@ -10,28 +11,45 @@ app.use(express.json());
 //Init file handler
 const tvFileHandler = new JSONFileHandler('./tvs.json');
 
-// Call init and load tv json file into memory
-let TVs = await init();
-
+// Call init to load files and initialize cron jobs
+let TVs = await initTVs();
+await initCron();
 
 
 // Mount the TV control API
 app.use('/api', expressAPI(TVs, tvFileHandler)); // Pass the TV data as an argument
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
 
 
-async function init() {
+async function initTVs() {
     try {
         const tvData = await tvFileHandler.readJSON();
         return tvData;
     } catch (error) {
         console.error(error);
         console.log("Creating new tvs.json file");
-        await tvFileHandler.saveJSON({ "tvs": [], "groups": []});
+        await tvFileHandler.saveJSON({ "tvs": [], "groups": [] });
         return {};
     }
+}
+
+function initCron() {
+    // Initialize cron jobs from JSON data
+    TVs.groups.forEach((job) => {
+        if (job.powerOn != "") {
+            cron.schedule(job.powerOn, () => {
+                //Power on all tvs in the group
+            });
+        }
+
+        if(job.powerOff != ""){
+            cron.schedule(job.powerOff, () => {
+                //Power off all tvs in the group
+            });
+        }
+    });
 }
 
