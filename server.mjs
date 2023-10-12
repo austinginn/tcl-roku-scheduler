a846028c63464c4324dae0dcde664b22688746e5
Austin Ginn <austinleeginn@gmail.com>
2023-10-12 01:20:19 +0000

    final commit before on site testing.  Includes solid logging to console but may want to use a specific package to create log files.

--- server.mjs
+++ server.mjs
@@ -3,6 +3,7 @@ import JSONFileHandler from "./json-file-handler.mjs";
 import express from 'express';
 import expressAPI from './expressAPI.mjs';
 import cron from 'node-cron';
+import cronstrue from 'cronstrue';
 
 const app = express();
 const port = 3000;
@@ -52,25 +53,38 @@ function initCron() {
         if (job.powerOn != "") {
             const onCron = cron.schedule(job.powerOn, () => {
                 //Power on all tvs in the group
+                //Logic for finding tvs in matching group
                 console.log("Powering on all tvs in group: " + job.name);
                 for(let i = 0; i < TVs.tvs.length; i++){
-                    console.log("Powering on tv: " + TVs.tvs[i].name);
+                    for(let x = 0; x < TVs.tvs.group.length; x++){
+                        if(TVs.tvs[i].group[x] == job.name){
+                            console.log("Powering on tv: " + TVs.tvs[i].name);
+                            TCLRokuTV.powerOn(TVs.tvs[i].ipAddress);
+                        }
+                    }
                 }
             });
-
             cronJobs[job.name + " - on"] = onCron;
+            console.log("Created power on job for: " + job.name + " - " + cronstrue.toString(job.powerOn));
         }
 
         if(job.powerOff != ""){
             const offCron = cron.schedule(job.powerOff, () => {
                 //Power off all tvs in the group
+                //Logic for finding tvs in matching group
                 console.log("Powering off all tvs in group: " + job.name);
                 for(let i = 0; i < TVs.tvs.length; i++){
-                    console.log("Powering off tv: " + TVs.tvs[i].name);
+                    for(let x = 0; x < TVs.tvs.group.length; x++){
+                        if(TVs.tvs[i].group[x] == job.name){
+                            console.log("Powering off tv: " + TVs.tvs[i].name);
+                            TCLRokuTV.powerOff(TVs.tvs[i].ipAddress);
+                        }
+                    }
                 }
             });
 
             cronJobs[job.name + " - off"] = offCron;
+            console.log("Created power off job for: " + job.name + " - " + cronstrue.toString(job.powerOff));
         }
     });
 }
